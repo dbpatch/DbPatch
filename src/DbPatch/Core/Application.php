007133084d7f70312b493f53675f557a6fe7e91a
Sandy Pleyte <sandy@ibuildings.nl>
2011-05-10 23:03:56 +0200

    Moved opts to application

--- src/DbPatch/Core/Application.php
+++ src/DbPatch/Core/Application.php
@@ -9,17 +9,31 @@
 
 class DbPatch_Core_Application
 {
+
+    protected $opts;
+
     public function main()
     {
         //@todo create database, inject config
+        $this->opts = new Zend_Console_Getopt($this->getRules());
+        try {
+            $action = $this->getAction();
+        }
+        catch(Zend_Console_Getopt_Exception $e)
+         {
+             echo 'ERROR: '.$e->getMessage() . PHP_EOL . PHP_EOL;
+             echo $this->getUsageMessage() . PHP_EOL;
+             exit;
+
+         }
 
         $runner = new DbPatch_Task_Runner();
-        $task = $runner->getTask();
+        $task = $runner->getTask($action);
 
         try
         {
 
-            if ($runner->getVerbose())
+            if ($this->getVerbose())
             {
               DbPatch_Core_Application::renderVersion();
             }
@@ -29,10 +43,66 @@ class DbPatch_Core_Application
         catch(Exception $e)
         {
             echo 'ERROR: '.$e->getMessage() . PHP_EOL . PHP_EOL;
-            echo $runner->getUsageMessage() . PHP_EOL;
+            echo $this->getUsageMessage() . PHP_EOL;
+            exit;
         }
 
 
+    }
+
+    public function getUsageMessage()
+    {
+       echo $this->opts->getUsageMessage();
+    }
+
+    public function getVerbose()
+    {
+        if (isset($this->opts->v)) {
+            return true;
+        }
+        return false;
+    }
+
+    protected function getAction()
+    {
+
+        if (!isset($this->opts->config)) {
+            throw new Zend_Console_Getopt_Exception('No config file set');
+        } elseif (isset($this->opts->install)) {
+            return 'install';
+        } elseif (isset($this->opts->reinstall)) {
+            return 'reinstall';
+        } elseif (isset($this->opts->remove)) {
+            return 'remove';
+        } elseif (isset($this->opts->status)) {
+            return 'status';
+        } elseif (isset($this->opts->sync)) {
+            return 'sync';
+        } elseif (isset($this->opts->upgrade)) {
+            return 'upgrade';
+        } else {
+            throw new Zend_Console_Getopt_Exception('Unknown action');
+        }
+    }
+
+
+    protected function getRules()
+    {
+        $rules = array(
+            'branch=s' => 'Branch',
+            'config=s' => 'Config file',
+            'help' => 'Displays usage information.',
+            'install' => 'Install database',
+            'patch=s' => 'Patch number',
+            'remove' => 'Remove patch',
+            'reinstall' => 'Reinstall database',
+            'skip=s' => 'Skip patch number',
+            'status' => 'Database patch status',
+            'sync'  => 'Sync database',
+            'upgrade' => 'Upgrade database',
+            'verbose' => 'Displays debug information.',
+        );
+        return $rules;
     }
 
     /**
--- src/DbPatch/Task/Runner.php
+++ src/DbPatch/Task/Runner.php
@@ -12,56 +12,12 @@ class DbPatch_Task_Runner
     private $opts = null;
 
     public function __construct()
+    {}
+
+    public function getTask($action)
     {
-        $rules = array(
-            'branch=s' => 'Branch',
-            'config=s' => 'Config file',
-            'help' => 'Displays usage information.',
-            'install' => 'Install database',
-            'patch=s' => 'Patch number',
-            'remove' => 'Remove patch',
-            'reinstall' => 'Reinstall database',
-            'skip=s' => 'Skip patch number',
-            'status' => 'Database patch status',
-            'sync'  => 'Sync database',
-            'upgrade' => 'Upgrade database',
-            'verbose' => 'Displays debug information.',
-        );
-        $this->opts = new Zend_Console_Getopt($rules);
-
-
-    }
-
-    protected function getAction()
-    {
-
-        if (!isset($this->opts->config)) {
-            throw new Zend_Console_Getopt_Exception('No config file set');
-        } elseif (isset($this->opts->install)) {
-            return 'install';
-        } elseif (isset($this->opts->reinstall)) {
-            return 'reinstall';
-        } elseif (isset($this->opts->remove)) {
-            return 'remove';
-        } elseif (isset($this->opts->status)) {
-            return 'status';
-        } elseif (isset($this->opts->sync)) {
-            return 'sync';
-        } elseif (isset($this->opts->upgrade)) {
-            return 'upgrade';
-        } else {
-            throw new Zend_Console_Getopt_Exception('Unknown action');
-        }
-    }
-
-
-
-    public function getTask()
-    {
-        try {
-            $this->opts->parse();
-            $action = $this->getAction();
-
+        $class = null;
+        
             switch($action) {
                 case 'install' : // install
                     $class = 'DbPatch_Task_Install';
@@ -87,21 +43,7 @@ class DbPatch_Task_Runner
             
             return $task;
 
-        } catch (Zend_Console_Getopt_Exception $e) {
-            throw new Exception($e->getMessage() );
-        }
     }
 
-    public function getUsageMessage()
-    {
-       echo $this->opts->getUsageMessage();
-    }
 
-    public function getVerbose()
-    {
-        if (isset($this->opts->v)) {
-            return true;
-        }
-        return false;
-    }
 }
