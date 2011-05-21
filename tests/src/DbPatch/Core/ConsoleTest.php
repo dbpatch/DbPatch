dce447a03fd0a9df54ff03d028dc8ce520db907c
Martijn De Letter <martijn@ibuildings.nl>
2011-05-21 11:05:32 +0200

    add extra console tests

--- tests/src/DbPatch/Core/ConsoleTest.php
+++ tests/src/DbPatch/Core/ConsoleTest.php
@@ -52,4 +52,29 @@ class ConsoleTest extends PHPUnit_Framework_TestCase
         $console = new DbPatch_Core_Console($argv);
         $this->assertEquals(array_slice($argv, 1), $console->getArguments());
     }
+    
+    public function testGetOptionValue()
+    {
+        $argv = array('./dbpatch.php', 'update', '--skip=2,3');
+        
+        $console = new DbPatch_Core_Console($argv);
+        $this->assertEquals('2,3', $console->getOptionValue('skip'));
+    }
+    
+    public function testGetOptionValueWithDefaultValue()
+    {
+        $argv = array('./dbpatch.php', 'update');
+        
+        $console = new DbPatch_Core_Console($argv);
+        $this->assertFalse($console->getOptionValue('force', false));
+    }
+    
+    public function testIssetOption()
+    {
+        $argv = array('./dbpatch.php', 'update', '--force');
+        
+        $console = new DbPatch_Core_Console($argv);
+        $this->assertTrue($console->issetOption('force'));
+        $this->assertFalse($console->issetOption('bogus'));
+    }
 }
