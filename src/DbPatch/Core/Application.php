77067c19d9e4c093a7f86ee58c3ffd0129c56528
Martijn De Letter <martijn@ibuildings.nl>
2011-06-06 11:37:30 +0200

    Added some phpdoc comments

--- src/DbPatch/Core/Application.php
+++ src/DbPatch/Core/Application.php
@@ -5,6 +5,10 @@
  */
 class DbPatch_Core_Application
 {
+    /**
+     * Initialize the dbpatch application
+     * Typically called from bin/dbpatch.php
+     */
     public function main()
     {
         $console    = $this->getConsole($_SERVER['argv']);
@@ -23,6 +27,7 @@ class DbPatch_Core_Application
             $writer->setVerbose();
         }
 
+        // Load the right config file
         try {
             $config  = $this->getConfig($configFile);
         } catch (Exception $e) {
@@ -38,6 +43,7 @@ class DbPatch_Core_Application
             return;
         }
 
+        // Finally execute the right task
         try {
             $task = $console->getTask();
             $runner->getTask($task, $console)
@@ -55,33 +61,67 @@ class DbPatch_Core_Application
         return;
     }
 
+    /**
+     * Returns the dbpatch logger object
+     *
+     * @return DbPatch_Core_Log
+     */
     protected function getLogger()
     {
         return new DbPatch_Core_Log();
     }
 
+    /**
+     * Initia
+     *
+     * @param DbPatch_Core_Writer $writer 
+     * @return DbPatch_Task_Runner
+     */
     protected function getTaskRunner($writer)
     {
         return new DbPatch_Task_Runner($writer);
     }
 
+    /**
+     * Try to load the right config file
+     *
+     * @param string $filename
+     * @return DbPatch_Core_Config
+     */
     protected function getConfig($filename = null)
     {
         $config = new DbPatch_Core_Config($filename);
         return $config->getConfig();
     }
 
+    /**
+     * DB object
+     *
+     * @param DbPatch_Core_Config $config
+     * @return DbPatch_Core_Db
+     */
     protected function getDb($config)
     {
         $db = new DbPatch_Core_Db($config);
         return $db->getDb();
     }
     
+    /**
+     * CLI writer
+     *
+     * @return DbPatch_Core_Writer
+     */
     protected function getWriter()
     {
         return new DbPatch_Core_Writer();
     }
 
+    /**
+     * Creates a console object
+     *
+     * @param array $argv
+     * @return DbPatch_Core_Writer
+     */
     protected function getConsole($argv)
     {
         return new DbPatch_Core_Console($argv);
