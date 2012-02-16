96f8ad4d26fef985e98f0d92bdbfec965d8b61a0
Sandy Pleyte <sandy@ibuildings.nl>
2012-02-16 21:27:03 +0100

    Added php config (contributed by Boy)

--- docs/dbpatch.php
+++ docs/dbpatch.php
@@ -1,5 +1,23 @@
 <?php
 
 $dbPatchConfig = array(
-
+    'limit' => 10,
+    'default_branch' => 'default',
+    'patch_directory' => '../patches',
+    'patch_prefix' => 'patch',
+    'color' => false,
+    'dump_before_update' => false,
+    'dump_directory' => '',
+    # database settings
+    'db' => array(
+        'adapter' => 'Mysqli',
+        'params' => array(
+            'host' => 'localhost',
+            'username' => 'user',
+            'password' => 'pass',
+            'dbname' => 'db',
+            'charset' => 'utf8',
+            'bin_dir' => '',
+        )
+    )
 );
